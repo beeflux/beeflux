88e33f847a4fc3b22b673b1d13a0dd4488050f01
Razyesh <pudasainirajesh504@gmail.com>
2019-05-11 09:36:58 +0545

    subscriptions model created

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+PACKAGE_CATEGORIES = (
+    (1,'Free Trial'),
+    (2,'Basic'),
+    (3,'Business'),
+    (4,'Professional')
+)
+
+class Package(models.Model):
+    category = models.IntegerField(choices=PACKAGE_CATEGORIES, default=1)
+    description = models.TextField(blank=True, null=True)
+    pricing = models.FloatField(max_length=100)
+
+    class Meta:
+        db_table = 'package'
+
+class Subscription(models.Model):
+    package = models.ForeignKey("Package",related_name="subscriptions",on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField(auto_now_add=False)
+
+    class Meta:
+        get_latest_by = ['-priority', 'order_date']
+
+    def __str__(self):
+        return self.package
+
+class Payment(models.Model):
+    subscription = models.ForeignKey("Subscription", related_name='subscription', on_delete=models.CASCADE)
+    pay = models.FloatField()
+
