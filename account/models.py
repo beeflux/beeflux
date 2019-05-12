a10a81c9aec2817832d7c04ebf4072d24029e515
Neha Pokharel <nehapokharel008@gmail.com>
2019-05-12 12:52:30 +0545

    company account created

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=250)
+    estd = models.DateTimeField(default=timezone.now)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    vat_number = models.CharField(max_length=40, blank=False, null=False)
+    address = models.CharField(max_length=300)
