e10c31f93563b7458178834b024494ce5ff79829
Razyesh <pudasainirajesh504@gmail.com>
2019-05-12 13:29:35 +0545

    created the model Post along with the access for Comment and Like into the post

--- beeflux/settings.py
+++ beeflux/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'post.apps.PostConfig',
     'subscription.apps.SubscriptionConfig',
     'core.apps.CoreConfig',
     'django.contrib.admin',
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+MEDIA_URL= "/media/"
